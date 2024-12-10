5088f84d97012592e16d1a60ecf5eb3df8ee686f
AlexanderVasilev629 <a.vasilev2020@pmg-pernik.com>
2024-12-10 18:07:51 +0200

    Upload project

---
+++ 03.DogsApp-4/03.DogsApp-4/DogsApp/DogsApp.Core/Contracts/IBreedService.cs
@@ -0,0 +1,11 @@
+﻿using DogsApp.Infrastructure.Data.Domain;
+
+namespace DogsApp.Core.Contracts
+{
+    public interface IBreedService
+    {
+        List<Breed> GetAllBreeds();
+        Breed GetBreedById(int breedId);
+        List<Dog> GetDogsByBreed(int breedId);
+    }
+}
---
+++ 03.DogsApp-4/03.DogsApp-4/DogsApp/DogsApp.Infrastructure/Data/ApplicaionBuilderExtention.cs
@@ -0,0 +1,45 @@
+﻿using DogsApp.Infrastructure.Data.Domain;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsApp.Infrastructure.Data
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static async Task<IApplicationBuilder> PrepareDatabase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+
+            var services = serviceScope.ServiceProvider;
+
+            var data = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            SeedBreeds(data);
+
+            return app;
+        }
+
+        public static void SeedBreeds(ApplicationDbContext data)
+        {
+            if (data.Breeds.Any())
+            {
+                return;
+            }
+            data.Breeds.AddRange(new[]
+            {
+                new Breed { Name = "Husky" },
+                new Breed { Name = "Pinscher" },
+                new Breed { Name = "Cocer spaniol" },
+                new Breed { Name = "Dachshund" },
+                new Breed { Name = "Doberman" },
+                new Breed { Name = "Chihuahua" },
+                new Breed { Name = "Rothweiler" },
+            });
+            data.SaveChanges();
+        }
+    }
+}
